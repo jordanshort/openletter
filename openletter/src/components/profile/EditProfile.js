d5983b349d7f1a29a0f760c43f23a75ea8640f1a
Jordan <jordan.short@gmail.com>
2018-02-15 11:20:49 -0700

    added profile edit page w/out styling

---
+++ openletter/src/components/profile/EditProfile.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Modal, Button, DropdownButton, MenuItem, ButtonToolbar, FormGroup, FormControl } from 'react-bootstrap';
+import '../../fontawesome-all';
+import './EditProfile.css';
+
+
+export default function EditProfile(props){
+    return(
+        <div className="static-modal">
+            <Modal show={true}>
+                <Modal.Header>
+                    <Modal.Title>Edit Your Profile</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div>Add A Profile Picture <i class="fas fa-camera fa-sm"></i> </div>
+                    <div className="birthday">
+                        <ButtonToolbar>
+                            <DropdownButton bsSize="small" title={ props.month || "Month"} id="month" onSelect={(val) => props.handleSelect('month', val)}>
+                                <MenuItem eventKey='January'>January</MenuItem>
+                                <MenuItem eventKey='February'>February</MenuItem>
+                                <MenuItem eventKey='March'>March</MenuItem>
+                                <MenuItem eventKey='April'>April</MenuItem>
+                                <MenuItem eventKey='May'>May</MenuItem>
+                                <MenuItem eventKey='June'>June</MenuItem>
+                                <MenuItem eventKey='July'>July</MenuItem>
+                                <MenuItem eventKey='August'>August</MenuItem>
+                                <MenuItem eventKey='September'>September</MenuItem>
+                                <MenuItem eventKey='October'>October</MenuItem>
+                                <MenuItem eventKey='November'>November</MenuItem>
+                                <MenuItem eventKey='December'>December</MenuItem>
+                            </DropdownButton>
+                            <DropdownButton bsSize="small" title={ props.day || "Day" } id="day" onSelect={(val) => props.handleSelect('day', val)}>
+                                <MenuItem eventKey='1'>1</MenuItem>
+                                <MenuItem eventKey='2'>2</MenuItem>
+                                <MenuItem eventKey='3'>3</MenuItem>
+                                <MenuItem eventKey='4'>4</MenuItem>
+                                <MenuItem eventKey='5'>5</MenuItem>
+                                <MenuItem eventKey='6'>6</MenuItem>
+                                <MenuItem eventKey='7'>7</MenuItem>
+                                <MenuItem eventKey='8'>8</MenuItem>
+                                <MenuItem eventKey='9'>9</MenuItem>
+                                <MenuItem eventKey='10'>10</MenuItem>
+                                <MenuItem eventKey='11'>11</MenuItem>
+                                <MenuItem eventKey='12'>12</MenuItem>
+                                <MenuItem eventKey='13'>13</MenuItem>
+                                <MenuItem eventKey='14'>14</MenuItem>
+                                <MenuItem eventKey='15'>15</MenuItem>
+                                <MenuItem eventKey='16'>16</MenuItem>
+                                <MenuItem eventKey='17'>17</MenuItem>
+                                <MenuItem eventKey='18'>18</MenuItem>
+                                <MenuItem eventKey='19'>19</MenuItem>
+                                <MenuItem eventKey='20'>20</MenuItem>
+                                <MenuItem eventKey='21'>21</MenuItem>
+                                <MenuItem eventKey='22'>22</MenuItem>
+                                <MenuItem eventKey='23'>23</MenuItem>
+                                <MenuItem eventKey='24'>24</MenuItem>
+                                <MenuItem eventKey='25'>25</MenuItem>
+                                <MenuItem eventKey='26'>26</MenuItem>
+                                <MenuItem eventKey='27'>27</MenuItem>
+                                <MenuItem eventKey='28'>28</MenuItem>
+                                <MenuItem eventKey='29'>29</MenuItem>
+                                <MenuItem eventKey='30'>30</MenuItem>
+                                <MenuItem eventKey='31'>31</MenuItem>
+                            </DropdownButton>
+                            <FormGroup validationState={props.getValidationState()}>
+                                <FormControl bsSize="small" type="text" value={props.year} placeholder="Enter 4 digit Year ex: 1986" onChange={(e) => props.handleChange('year', e.target.value)} />
+                            </FormGroup>
+                        </ButtonToolbar>
+                        <div>
+                            Add A Summary About You
+                            <input className="about-input" type="text"  value={props.about} onChange={(e) => props.handleChange('about', e.target.value)}/>
+                        </div>
+                        <div>
+                            Add Your Job Title 
+                            <input className="job-input" type="text"  value={props.job} onChange={(e) => props.handleChange('job', e.target.value)}/>                            
+                        </div>
+                        <div>
+                            Add Your Employer 
+                            <input className="employer-input" type="text"  value={props.employer} onChange={(e) => props.handleChange('employer', e.target.value)}/>                            
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button>Save</Button>
+                    <Button bsStyle="danger" onClick={props.handleClose}>Cancel</Button>
+                </Modal.Footer>
+            </Modal>
+
+        </div>
+    )
+}
--- openletter/src/components/profile/Profile.js
+++ openletter/src/components/profile/Profile.js
@@ -4,16 +4,56 @@ import './Profile.css';
 import LetterCard from '../letterCard/LetterCard';
 import { connect } from 'react-redux';
 import { fetchUser } from '../../redux/reducer';
+import EditProfile from './EditProfile';
 
 class Profile extends Component{
+    constructor(){
+        super();
+        this.state = {
+            show: false,
+            month: '',
+            day: '',
+            year: '',
+            about: '',
+            job: '',
+            employer: ''
+        };
+        this.handleClose = this.handleClose.bind(this);
+        this.handleShow = this.handleShow.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.getValidationState = this.getValidationState.bind(this);
+    }
 
     componentDidMount(){
-        this.props.fetchUser();
+        // this.props.fetchUser();
+    }
+
+    handleClose(){
+        this.setState({show: false});
+    }
+
+    handleShow(){
+        this.setState({show: true});
+    }
+
+    handleSelect(name, val){
+        this.setState({[name]: val});
+    }
+
+    handleChange(name, val){
+        this.setState({[name]: val});
+    }
+
+    getValidationState(){
+        const length = this.state.year.length;
+        if (length === 4) return 'sucess';
+        else return 'error';
     }
 
 
     render(){
-        if (!this.props.user){return null};
+        // if (!this.props.user){return null};
         const { user } = this.props;
         return(
             <div className="user-profile-root">
@@ -21,17 +61,45 @@ class Profile extends Component{
                 <div className="user-profile-body-container">
                     <div className="user-profile-card">
                         <div className="user-profile-picture">
-                            <img src={user.picture} alt=""/>
+                            { user.picture ? 
+                                <img src={user.picture} alt=""/>                            
+                                : 'Add a picture'}
                         </div>
                         <div className="user-profile-name">{user.first_name} {user.last_name}</div>
-                        <div className="user-profile-birthday">{user.birth_month}{user.birth_day}, {user.birth_year}</div>
-                        <div className="user-profile-about">{user.about_me}</div>
-                        <div className="user-profile-job">{user.job}</div>
-                        <div className="user-profile-employer">{user.employer}</div>
-                        <button className="btn">Follow</button>
+                        <div className="user-profile-birthday">
+                            { user.birth_month ?
+                                `${user.birth_month} ${user.birth_day}, ${user.birth_year}`
+                                : 'Add your birthday'}
+                        </div>
+                        <div className="user-profile-about">
+                            {user.about_me ?
+                                `${user.about_me}`
+                                : 'Add a summary about you'
+                            }</div>
+                        <div className="user-profile-job">
+                            {user.job ? 
+                                `${user.job}`
+                                : 'Add your job title'}
+                            </div>
+                        <div className="user-profile-employer">
+                            {user.employer ? 
+                                `${user.employer}`
+                                : 'Add your employer'}
+                            </div>
+                        <button className="btn" onClick={this.handleShow}>Edit Profile</button>
                     </div>
                     
                 </div>
+                <EditProfile 
+                    handleSelect={this.handleSelect} 
+                    show={this.state.show} 
+                    handleClose={this.handleClose} 
+                    month={this.state.month} 
+                    day={this.state.day} 
+                    year={this.state.year}
+                    handleChange={this.handleChange}
+                    getValidationState={this.getValidationState}
+                    />
             </div>
         )
     }
