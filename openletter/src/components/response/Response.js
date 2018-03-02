3fc8467815bbe9e423e159ebd48bb36e052e08c8
Jordan <jordan.short@gmail.com>
2018-03-02 13:37:02 -0700

    added cancel warning to composing responses

--- openletter/src/components/response/Response.js
+++ openletter/src/components/response/Response.js
@@ -5,6 +5,7 @@ import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import { connect } from 'react-redux';
 import { postResponse } from '../../redux/reducer';
+import { Modal } from 'react-bootstrap';
 
 
 class Response extends Component{
@@ -12,6 +13,7 @@ class Response extends Component{
         super();
         this.state = {
             text: '',
+            showModal: false
         };
         this.handleChange = this.handleChange.bind(this);
     };
@@ -61,10 +63,19 @@ class Response extends Component{
                              />
                              <div className="letter-buttons">
                                 <button className="btn btn-default" onClick={() => this.onSubmit()}>Send</button>
-                                <button onClick={() => this.props.history.goBack()} className="btn btn-danger">Cancel</button>
+                                <button onClick={() => this.setState({showModal: true})} className="btn btn-danger">Cancel</button>
                              </div>
                     </div>
                 </div>
+                <Modal show={this.state.showModal}>
+                    <Modal.Body>
+                        All your work will be deleted.  Are you sure you still want to exit?
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button className="btn btn-default" onClick={() => this.setState({showModal: false})}>Continue Writing</button>
+                        <button className="btn btn-danger" onClick={() => this.props.history.goBack()}>Yes, Exit</button>
+                    </Modal.Footer>
+                </Modal>
             </div>
         )
     }
